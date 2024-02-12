7daba762ea650757f9d3cc9e9534053578119177
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2024-02-12 19:05:56 +0400

    server working end

--- index.js
+++ index.js
@@ -2,36 +2,6 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// Replace with your MongoDB connection string
-const mongoURI =
-  "mongodb+srv://firstMongoDb:narsa1997@cluster0.cgkga1s.mongodb.net/?retryWrites=true&w=majority";
-
-// Connect to MongoDB
-mongoose
-  .connect(mongoURI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
-
-// Define User schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-const User = mongoose.model("User", userSchema);
-
 const app = express();
 app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
@@ -41,23 +11,55 @@ app.get("/", (req, res) => {
   res.send("Hello, this is the home page!");
 });
 
+const mongoURI =
+  "mongodb+srv://firstMongoDb:narsa1997@cluster0.cgkga1s.mongodb.net?retryWrites=true&w=majority";
+// Connect to MongoDB in cluster0
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("conect mongoDB"))
+  .catch((err) => console.error(err));
+
+// Connect to MongoDB in cluster0 in databeiz Users
+
+const dbUsers = mongoose.connection.useDb("Users");
+const user = dbUsers.model(
+  "user",
+  new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  }),
+  "users-register"
+);
+
 app.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   console.log("Received data:", req.body);
   console.log("Received data:", username, email, password);
 
-  // Basic validation (replace with more thorough checks)
+  //  validation
   if (!username || !email || !password) {
     return res.status(400).json({ error: "Please fill in all fields" });
   }
 
   try {
-    const existingUser = await User.findOne({ username });
+    const existingUser = await user.findOne({ username });
     if (existingUser) {
       return res.status(400).json({ error: "Username already exists" });
     }
 
-    const newUser = new User({ username, email, password });
+    const newUser = new user({ username, email, password });
     await newUser.save();
 
     res.status(201).json({ message: "Registration successful!" });
@@ -67,28 +69,19 @@ app.post("/register", async (req, res) => {
   }
 });
 
+const findEdUser = "nikala10";
+
+try {
+  const findUser = await user.findOne({ username: findEdUser });
+
+  if (findUser) {
+    console.log("find user", findUser);
+  } else {
+    console.log("user notFound");
+  }
+} catch (err) {
+  console.error(err);
+}
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running  http://localhost:${port}`));
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json()); // Middleware to parse JSON body
-
-// app.get("/", (req, res) => {
-//   res.send("Hello, this is the home page!");
-// });
-
-// app.post("/register", async (req, res) => {
-//   console.log("Received data:", req.body);
-//   const { username, email, password } = req.body;
-
-//   // Perform validation and data processing here (replace with your logic)
-//   console.log("Received data:", username, email, password);
-
-//   // Interact with database or perform further actions
-
-//   res.status(201).json({ message: "Registration successful!" }); // Replace with appropriate response
-// });
-
-// const port = process.env.PORT || 4000;
-// app.listen(port, () => console.log(`Server running  http://localhost:${port}`));
